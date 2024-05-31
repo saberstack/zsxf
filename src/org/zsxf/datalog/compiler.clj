5eb340c548eef845a83cf64c0bb9a66875ed62c3
Rangel Spasov <raspasov@gmail.com>
2024-05-31 02:26:24 -0700

    Require pattern library in datalog compiler

--- src/org/zsxf/datalog/compiler.clj
+++ src/org/zsxf/datalog/compiler.clj
@@ -1,1 +1,8 @@
-(ns org.zsxf.datalog.compiler)
+(ns org.zsxf.datalog.compiler
+  (:require [pattern]))
+
+(defn init []
+  (pattern/compile-pattern '[?before 1 2 3 ?after]))
+
+(comment
+  ((init) [0 1 2 3 4]))

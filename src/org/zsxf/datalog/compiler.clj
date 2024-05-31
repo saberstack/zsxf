2d68eadd6e842ac3dbe18dea188bc3a97a80766c
Rangel Spasov <raspasov@gmail.com>
2024-05-31 04:59:28 -0700

    Extend datalog compiler functionality
    The datalog compiler has been expanded to include additional features. This includes new patterns compiled by the pattern module and a function to transform datalog to zsxf. Also, some adjustments have been made to improve readability and maintainability, such as aliasing the 'pattern' requirement to 'p'.

--- src/org/zsxf/datalog/compiler.clj
+++ src/org/zsxf/datalog/compiler.clj
@@ -1,8 +1,32 @@
 (ns org.zsxf.datalog.compiler
-  (:require [pattern]))
+  (:require [pattern :as p]))
 
 (defn init []
-  (pattern/compile-pattern '[?before 1 2 3 ?after]))
+  (p/compile-pattern '[?before 1 2 3 ?after]))
 
 (comment
   ((init) [0 1 2 3 4]))
+
+(def m (p/compile-pattern '[?fn-name-1 (fn ?fn-name-2 [??args] ??body)]))
+
+(m ['abc '(fn abc [x y z] x)])                              ; => {:fn-name 'abc ...}
+(m ['xyz '(fn abc [x] x)])                                  ; => nil
+
+(def example-datalog-query-1
+  '[:find (pull ?exercise-set-eids [:db/id])
+    :in $ ?workout-uuid
+    :where
+    [?workout-eid :workout/uuid ?workout-uuid]
+    [?exercise-set-eids :exercise-set/workout ?workout-eid]])
+
+
+(def datalog->zsxf-matcher
+  (p/compile-pattern
+    '[??_
+      :where ??clauses]))
+
+(defn datalog->zsxf [query]
+  (datalog->zsxf-matcher query))
+
+(comment
+  (datalog->zsxf example-datalog-query-1))

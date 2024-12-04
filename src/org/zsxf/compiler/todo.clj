882e6b93c4e3fa57d48d03ce88603f63801b731a
Rangel Spasov <raspasov@gmail.com>
2024-12-04 12:22:54 -0800

    org.zsxf.compiler.todo

---
+++ src/org/zsxf/compiler/todo.clj
@@ -0,0 +1,8 @@
+(ns org.zsxf.compiler.todo)
+
+
+;TODO in order
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; 1. Transform :where to directed acyclic graph (DAG)
+; 2. Transform DAG to code similar to example in org.zsxf.experimental.dataflow/incremental-computation-xf
+; 3. PoC query demo: datalog->dag->code

88a0875e4ec5b7229c75114651bb473e78b22a47
Rangel Spasov <raspasov@gmail.com>
2024-12-04 13:47:43 -0800

    Macro vs anonymous fn discussion

--- src/org/zsxf/compiler/todo.clj
+++ src/org/zsxf/compiler/todo.clj
@@ -5,4 +5,5 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; 1. Transform :where to directed acyclic graph (DAG)
 ; 2. Transform DAG to code similar to example in org.zsxf.experimental.dataflow/incremental-computation-xf
+;  - either macro or anonymous fn calls
 ; 3. PoC query demo: datalog->dag->code

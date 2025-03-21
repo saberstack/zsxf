250004c9c40752b5d849eeb99790cefa6b76b8cd
Nick McAvoy <ohbadiah@outlook.com>
2025-03-21 19:40:32 -0400

    WIP test

--- test/org/zsxf/datascript_test.clj
+++ test/org/zsxf/datascript_test.clj
@@ -1,11 +1,16 @@
 (ns org.zsxf.datascript-test
   (:require
+   [clojure.core.async :as a]
    [clojure.test :refer [deftest is]]
    [clojure.edn :as edn]
    [clojure.java.io :as io]
    [datascript.core :as d]
-   [org.zsxf.datascript :as subject]
-   [org.zsxf.experimental.datastream :as ds])
+   [org.zsxf.datascript :as ds]
+   [org.zsxf.zset :as zs]
+   [org.zsxf.xf :as xf]
+   [org.zsxf.experimental.datastream :as data-stream]
+   [net.cgrand.xforms :as xforms]
+   [taoensso.timbre :as timbre])
   (:import
    [java.io PushbackReader]))
 
@@ -13,14 +18,57 @@
 (defn load-edn-file [file-path]
   (with-open [rdr (io/reader file-path)]
     (edn/read (PushbackReader. rdr))))
-(def ra (atom []))
-(defn load-learn-db []
-  (let [schema (load-edn-file "resources/learndatalogtoday/schema_datascript.edn")
-        data (load-edn-file   "resources/learndatalogtoday/data_datascript.edn")
-        conn (d/create-conn schema)]
-    (ds/listen-datom-stream conn ra)
-    (d/transact! conn data)
-    conn))
+
+(defn load-learn-db
+  ([]
+   (load-learn-db nil)
+   )
+  ([listen-atom]
+   (let [schema (load-edn-file "resources/learndatalogtoday/schema_datascript.edn")
+         data (load-edn-file   "resources/learndatalogtoday/data_datascript.edn")
+         conn (d/create-conn schema)]
+     (when listen-atom
+       (data-stream/listen-datom-stream conn listen-atom))
+     (d/transact! conn data)
+     conn)))
+
+
+(deftest test-robocop "basic datalog query"
+  (let [input (a/chan)
+        txn-atom (atom [])
+        index-state-all (atom [])
+        conn (load-learn-db txn-atom)
+        result-set (atom [])
+        xf (comp
+            (xf/mapcat-zset-transaction-xf)
+            (let [pred-1 #(ds/datom-attr= % :person/name)
+                  pred-2 #(ds/datom-attr= % :movie/director)
+                  pred-3 #(ds/datom-attr= (second %) :movie/director)
+                  pred-4 #(and (ds/datom-attr= % :movie/title) (ds/datom-val= % "RoboCop"))]
+              (comp
+               (map (fn [zset]
+                      (xf/disj-irrelevant-items
+                       zset pred-1 pred-2 pred-3 pred-4)))
+               (map (fn [tx-current-item] (timbre/spy tx-current-item)))
+               (xf/join-xf
+                pred-1 ds/datom->eid
+                pred-2 ds/datom->val
+                index-state-all)
+               (map (fn [zset-in-between] (timbre/spy zset-in-between)))
+               (xf/join-xf
+                pred-3 #(-> % second (ds/datom->eid))
+                pred-4 ds/datom->eid
+                index-state-all
+                :last? true)
+               (map (fn [zset-in-between-last] (timbre/spy zset-in-between-last)))
+               (xforms/reduce zs/zset+))))
+          output-ch (a/chan (a/sliding-buffer 1)
+                      (xf/query-result-set-xf result-set))
+          _        (a/pipeline 1 output-ch xf input)]
+    (a/>!!
+     input
+     @txn-atom))
+  (is true))
 
 (comment [:find ?name
    :where

e754fef620f398808bb6cbd5150f930c85046239
Rangel Spasov <raspasov@gmail.com>
2025-03-29 01:06:50 -0700

    pull-join-xf comment explanation

--- src/org/zsxf/xf.clj
+++ src/org/zsxf/xf.clj
@@ -196,3 +196,32 @@
     conj
     #{}
     zset))
+
+(defn pull-join-xf []
+  ; A pull pattern is similar to a join but differs in important ways.
+  ; Given a query like:
+  '[:find (pull ?p [:person/name
+                    {:person/born [:country/name]}])
+    :where
+    [?p :person/name ?name]
+    [?m :movie/title _]
+    [?m :movie/director ?p]]
+  ; ... we can see that the pull starts as typical at ?p (entity id)
+  ; It _pulls_ the following:
+  ;   :person/name  – which was already requested in the main :where join
+  ;   :person/born  - new piece of data... which is actually a ref (a join!)
+  ;      ... which points to ...
+  ;   :country/name - new data also.
+  ;
+  ; (!) An important difference between a :where join and a (pull ...) "join":
+  ;
+  ; Even when a certain (pull ?p ...) "lacks" any (or all!) of the data requested by the (pull ...) pattern
+  ; the number of returned items in the result does not change (the query can return a list like
+  '([nil] [nil])                                            ; ... if nothing in the pattern is found
+  ; or if found (more typically):
+  '([#:person{:born #:country{:name "USA"}}]
+    [#:person{:born #:country{:name "Monaco"}}])
+  ;
+  ; More important differences likely exist, this is WIP.
+  ;
+  )

52a1f8ff88e7b17713ba6d50c6ceedf2071aff7b
Nick McAvoy <ohbadiah@outlook.com>
2025-04-01 16:44:52 -0400

    Implemented find var reduction, and cleaned up
    Rename macro too

--- test/org/zsxf/datascript_test.clj
+++ test/org/zsxf/datascript_test.clj
@@ -34,6 +34,15 @@
        (drop-while #(not= :where %))
        (drop  1)))
 
+(defn query->map [q]
+  (second (reduce
+           (*fn [[current-segment acc] el]
+                (if (keyword? el)
+                  [el acc]
+                  [current-segment (update acc current-segment (fnil conj []) el)]))
+           [nil {}]
+           q)))
+
 (defn name-clauses
   "Takes the where clauses of a datalog query and gives each a name, e.g. :c1.
   Returns a map from name to clause."
@@ -87,94 +96,14 @@
 (defn safe-second [thing]
   (when (vector? thing)
     (second thing)))
-                                        ;(clause-pred-macro [safe-first safe-second identity] [?m :movie/cast ?p])
+
 (defmacro clause-pred-macro [locator-vec [e a v :as clause]]
   (if (parser/variable? v)
     `#(ds/datom-attr=  ((comp ~@locator-vec) %) ~a)
     `#(ds/datom-attr-val= ((comp ~@locator-vec) %) ~a ~v)))
 
-(defn clause-pred [locator [e a v]]
-  (if (parser/variable? v)
-    #(ds/datom-attr= (locator %) a)
-    #(ds/datom-attr-val= (locator %) a v)))
-
-(defn where-xf [datalog-query]
-  (fn [state]
-    (let [where-clauses (query->where-clauses datalog-query)
-          ;; Give each clause a shorthand name, eg :c1
-          named-clauses (name-clauses where-clauses)
-          ;; For each variable, tell me which clauses it appears in, and in which position.
-          variable-index (index-variables named-clauses)
-          ;; For each clause, tell me the other clauses it leads to, and by what variable.
-          adjacency-list (build-adjacency-list named-clauses)
-          ;; Process that to just a list of e.g. [c1 c2] connected clauses
-          adjacency-tuples (for [[from-node destinations] adjacency-list
-                                 [to-node _] destinations];
-                             [from-node to-node])
-          ;; Just pick any clause to start with
-          [first-clause & remaining-clauses] (keys named-clauses)]
-      (loop [preds []
-             xf-steps []
-             covered-nodes #{first-clause}
-             remaining-nodes (set remaining-clauses)
-             ;; Locators tell me for a given clause where to find it in the joined tuple
-             ;; So if the current join looks like [[[c1 c2] c3] c4]
-             ;; The locator is~ {c1 (-> % first first first)
-             ;;                  c2 (-> % first first second)
-             ;;                  c3 (-> % first second)
-             ;;                  c4 (-> % second)}
-             locators {first-clause identity}
-             n 1]
-
-        (cond (empty? remaining-nodes)
-              ;; We won! Prepend and append the joins with the boilerplate.
-              (apply comp (concat
-                           [(xf/mapcat-zset-transaction-xf)
-                            (map (fn [zset]
-                                   (apply xf/disj-irrelevant-items (cons zset preds))))]
-                           xf-steps
-                           [(xforms/reduce zs/zset+)]))
-
-              ;; Stack overflow
-              (> n 10)
-              n
-
-              :else
-              (let [; Find me the first edge from any clause already included
-                                        ; to any clause that hasn't been included yet.
-                    [from to :as edge] (medley/find-first
-                                        (fn [[from to]]
-                                          (and (covered-nodes from)
-                                               (remaining-nodes to)))
-                                        adjacency-tuples)
-                    ;; Look up what the variable is that links these clauses.
-                    common-var (get-in adjacency-list [from to])
-                    ;; Dereference the clause names to the clauses themselves.
-                    [c1 c2] (map named-clauses edge)
-                    ;; Generate predicates for checking join conditions.
-                    [p1 p2] [(clause-pred (from locators) c1) (clause-pred identity c2)]
-                    new-join (xf/join-xf p1
-                                         ;; Index keys are just the clause locator composed with the getter from the datom.
-                                         (comp (get-in variable-index [common-var from]) (from locators))
-                                         p2
-                                         ;; Look up the getter out of the datom.
-                                         (get-in variable-index [common-var to])
-                                         state
-                                         ;; It's the last join when this is the last clause.
-                                         :last? (= #{to} remaining-nodes))]
-                (recur (conj preds p1 p2)
-                       (conj xf-steps new-join)
-                       (conj covered-nodes to)
-                       (disj remaining-nodes to)
-                       ;; As we lift this join to the left hand of the next step,
-                       ;; all of the locators need to be composed with `safe-first`.
-                       (-> (medley/map-vals #(comp % safe-first) locators)
-                           (assoc to safe-second)
-                           )
-                       (inc n))))))))
-
-(defmacro where-xf-macro [datalog-query]
-  (let [where-clauses# (query->where-clauses datalog-query)
+(defmacro sprinkle-dbsp-on [datalog-query]
+  (let [{where-clauses# :where find-vars# :find} (query->map datalog-query)
         named-clauses# (name-clauses where-clauses#)
         variable-index# (index-variables named-clauses#)
         adjacency-list# (build-adjacency-list named-clauses#)
@@ -197,7 +126,12 @@
                 (map (fn [zset#]
                        (xf/disj-irrelevant-items zset# ~@preds#)))
                 ~@xf-steps#
-                (xforms/reduce zs/zset+)))
+                (xforms/reduce (zs/zset-xf+ (map
+                                             (xf/with-meta-f
+                                               (juxt ~@(map (fn [find-var#]
+                                                              (let [[[clause-to-select# getter#] & _] (find-var# variable-index#)]
+                                                                `(comp ~getter# ~@(clause-to-select# locators#))))
+                                                            find-vars#))))))))
 
             ;; Stack overflow
             (> n# 10)
@@ -211,7 +145,7 @@
                                          adjacency-tuples#)
 
                   common-var# (get-in adjacency-list# [from# to#])
-                  [ [e1# a1# v1# :as c1#] [e2# a2# v2# :as c2#]] (map named-clauses# edge#)
+                  [[_ a1# v1# ] [_ a2# v2#]] (map named-clauses# edge#)
                   p1 (if (parser/variable? v1#)
                        `#(ds/datom-attr=  ((comp  ~@(from# locators#)) %) ~a1#)
                        `#(ds/datom-attr-val= ((comp ~@(from# locators#)) %) ~a1# ~v1#))
@@ -232,99 +166,57 @@
                          (assoc to# [safe-second]))
                      (inc n#)))))))
 
-(deftest test-b "another ad-hoc query"
-  (let [txn-atom (atom [])
-        _conn           (load-learn-db txn-atom)
-        query           (q/create-query
-                         (where-xf-macro [:find ?title ?year
-                                          :where
-                                          [?m :movie/title ?title]
-                                          [?m :movie/year ?year]]))]
-    (is
-     (= (transduce
-         (map (juxt (comp ds/datom->val first) (comp ds/datom->val second) ))
-         (completing #(conj %1 %2))
-         #{}
-         (q/input query @txn-atom))
-        #{["Lethal Weapon" 1987] ["Aliens" 1986]
-          ["The Terminator" 1984] ["Rambo: First Blood Part II" 1985]
-          ["Mad Max Beyond Thunderdome" 1985] ["Mad Max" 1979]
-          ["First Blood" 1982] ["Predator" 1987]
-          ["Terminator 2: Judgment Day" 1991] ["Predator 2" 1990]
-          ["Mad Max 2" 1981] ["Lethal Weapon 2" 1989]
-          ["Braveheart" 1995] ["Terminator 3: Rise of the Machines" 2003]
-          ["Commando" 1985] ["Die Hard" 1988]
-          ["Alien" 1979] ["RoboCop" 1987]
-          ["Rambo III" 1988] ["Lethal Weapon 3" 1992]}))))
-
 (deftest test-robocop-with-query-api "basic datalog query, with internal query api"
   (let [txn-atom (atom [])
         _conn           (load-learn-db txn-atom)
         query           (q/create-query
-                         (where-xf-macro [:find ?name
-                                          :where
-                                          [?p :person/name ?name]
-                                          [?m :movie/title "RoboCop"]
-                                          [?m :movie/director ?p]]))]
-    (is
-     (= (q/input query @txn-atom)
-        (q/get-result query)
-        #{^#:zset{:w 1}
-          [[[16 :person/name "Paul Verhoeven"]
-            [59 :movie/director 16]]
-           [59 :movie/title "RoboCop"]]}))))
+                         (sprinkle-dbsp-on [:find ?name
+                                            :where
+                                            [?p :person/name ?name]
+                                            [?m :movie/title "RoboCop"]
+                                            [?m :movie/director ?p]]))]
+    (is (= (q/input query @txn-atom)
+           (q/get-result query)
+           #{["Paul Verhoeven"]}))))
 
 (deftest test-ahhnold "Another basic query"
   (let [txn-atom (atom [])
         _conn (load-learn-db txn-atom)
         query (q/create-query
-               (where-xf-macro
+               (sprinkle-dbsp-on
                 [:find ?name
                  :where
                  [?m :movie/cast ?p]
                  [?p :person/name "Arnold Schwarzenegger"]
                  [?m :movie/director ?d]
                  [?d :person/name ?name]]))]
+    (is (= #{["Mark L. Lester"]
+             ["Jonathan Mostow"]
+             ["John McTiernan"]
+             ["James Cameron"]}
+           (q/input query @txn-atom)))))
 
-    (is (= #{"Jonathan Mostow" "James Cameron" "John McTiernan" "Mark L. Lester"}
-           (transduce
-            (map (comp ds/datom->val second))
-            (completing #(conj %1 %2))
-            #{}
-            (q/input query @txn-atom))))))
-
-(deftest test-function-vs-macro "where-xf versions"
+(deftest test-b "another ad-hoc query"
   (let [txn-atom (atom [])
-        _conn (load-learn-db txn-atom)
-        query1 (q/create-query
-               (where-xf-macro
-                [:find ?name
-                 :where
-                 [?m :movie/cast ?p]
-                 [?p :person/name "Arnold Schwarzenegger"]
-                 [?m :movie/director ?d]
-                 [?d :person/name ?name]]))
-        query2 (q/create-query
-                (where-xf
-                 '[:find ?name
-                   :where
-                   [?m :movie/cast ?p]
-                   [?p :person/name "Arnold Schwarzenegger"]
-                   [?m :movie/director ?d]
-                   [?d :person/name ?name]]))
-        get-result-set (fn [query ipt]
-                         (transduce
-                          (map (comp ds/datom->val second))
-                          (completing #(conj %1 %2))
-                          #{}
-                          (q/input query ipt)))]
-
-    (is (= #{"Jonathan Mostow" "James Cameron" "John McTiernan" "Mark L. Lester"}
-           (get-result-set query1 @txn-atom)
-           (get-result-set query2 @txn-atom)))))
+        _conn           (load-learn-db txn-atom)
+        query           (q/create-query
+                         (sprinkle-dbsp-on [:find ?title ?year
+                                            :where
+                                            [?m :movie/title ?title]
+                                            [?m :movie/year ?year]]))]
+    (is (= (q/input query @txn-atom)
+           #{["Lethal Weapon" 1987] ["Aliens" 1986]
+             ["The Terminator" 1984] ["Rambo: First Blood Part II" 1985]
+             ["Mad Max Beyond Thunderdome" 1985] ["Mad Max" 1979]
+             ["First Blood" 1982] ["Predator" 1987]
+             ["Terminator 2: Judgment Day" 1991] ["Predator 2" 1990]
+             ["Mad Max 2" 1981] ["Lethal Weapon 2" 1989]
+             ["Braveheart" 1995] ["Terminator 3: Rise of the Machines" 2003]
+             ["Commando" 1985] ["Die Hard" 1988]
+             ["Alien" 1979] ["RoboCop" 1987]
+             ["Rambo III" 1988] ["Lethal Weapon 3" 1992]}))))
 
 (comment
   (set! *print-meta* false)
-
 
   )

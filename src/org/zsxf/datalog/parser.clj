775019de36680d5ff8196225bef697ffbdde26c2
Nick McAvoy <ohbadiah@outlook.com>
2025-04-02 16:53:14 -0400

    Move parsing code into parser
    This involves a bit more indirection in the macro.
    
    The function `index-variables` previously associated with each variable
    in each clause a function that would get that variable out of the
    clause.
    
    This to me goes slightly beyond the work of a parser. The parser should
    just say what position the variable is in. It's then the work of the
    compiler to do the right thing. It's just a simple layer of indirection
    from a keyword denoting the position to a function for getting something
    in that position, but it seemed right.
    
    It might be a bit pedantic, but I thought it was worth it for the
    practice at macro-fu.

--- src/org/zsxf/datalog/parser.clj
+++ src/org/zsxf/datalog/parser.clj
@@ -7,3 +7,58 @@
    False otherwise."
   [x]
   (and (symbol? x) (string/starts-with? (str x) "?")))
+
+(defn query->map [q]
+  (second (reduce
+           (fn [[current-segment acc] el]
+                (if (keyword? el)
+                  [el acc]
+                  [current-segment (update acc current-segment (fnil conj []) el)]))
+           [nil {}]
+           q)))
+
+
+(defn name-clauses
+  "Takes the where clauses of a datalog query and gives each a name, e.g. :c1.
+  Returns a map from name to clause."
+  [where-clauses]
+  (into {}
+        (map-indexed (fn [idx el]
+                       [(keyword (format "c%s" (inc idx))) el]))
+        where-clauses))
+
+(defn index-variables
+  "Builds a map from each variable appearing in the where clauses
+  to the clauses they appear in, and in which position.
+  Returns map from variable to map from clause name to positional getter function."
+  [named-clauses]
+  (reduce
+   (fn [acc [clause-name [e _ v]]]
+     (cond-> acc
+       (variable? e)
+       (update e (fnil assoc {}) clause-name :entity)
+       (variable? v)
+       (update v (fnil assoc {}) clause-name :value)))
+   {}
+   named-clauses))
+
+(defn build-adjacency-list
+  "Where clauses in a datalog query can be modeled as the nodes
+  of a graph. The edges are common variables between clauses.
+  Returns a map from source clause name to a map from destination
+  clause name to the common variable (edge)."
+  [named-clauses]
+  (let [variable-index (index-variables named-clauses)]
+    (reduce
+     (fn [acc [clause-name [e _ v]]]
+       (assoc acc clause-name
+              (into {} (concat
+                        (when (variable? e)
+                          (for [dest (->> variable-index e keys (filter (partial not= clause-name)))]
+                            [dest e]))
+
+                        (when (variable? v)
+                          (for [dest (->> variable-index v keys (filter (partial not= clause-name)))]
+                            [dest v]))))))
+     {}
+     named-clauses)))
--- test/org/zsxf/datascript_test.clj
+++ test/org/zsxf/datascript_test.clj
@@ -29,59 +29,9 @@
      (d/transact! conn data)
      conn)))
 
-(defn query->map [q]
-  (second (reduce
-           (fn [[current-segment acc] el]
-                (if (keyword? el)
-                  [el acc]
-                  [current-segment (update acc current-segment (fnil conj []) el)]))
-           [nil {}]
-           q)))
-
-(defn name-clauses
-  "Takes the where clauses of a datalog query and gives each a name, e.g. :c1.
-  Returns a map from name to clause."
-  [where-clauses]
-  (into {}
-        (map-indexed (fn [idx el]
-                       [(keyword (format "c%s" (inc idx))) el]))
-        where-clauses))
-
-(defn index-variables
-  "Builds a map from each variable appearing in the where clauses
-  to the clauses they appear in, and in which position.
-  Returns map from variable to map from clause name to positional getter function."
-  [named-clauses]
-  (reduce
-   (fn [acc [clause-name [e _ v]]]
-     (cond-> acc
-       (parser/variable? e)
-       (update e (fnil assoc {}) clause-name ds/datom->eid )
-       (parser/variable? v)
-       (update v (fnil assoc {}) clause-name ds/datom->val)))
-   {}
-   named-clauses))
-
-(defn build-adjacency-list
-  "Where clauses in a datalog query can be modeled as the nodes
-  of a graph. The edges are common variables between clauses.
-  Returns a map from source clause name to a map from destination
-  clause name to the common variable (edge)."
-  [named-clauses]
-  (let [variable-index (index-variables named-clauses)]
-    (reduce
-     (fn [acc [clause-name [e _ v]]]
-       (assoc acc clause-name
-              (into {} (concat
-                        (when (parser/variable? e)
-                          (for [dest (->> variable-index e keys (filter (partial not= clause-name)))]
-                            [dest e]))
-
-                        (when (parser/variable? v)
-                          (for [dest (->> variable-index v keys (filter (partial not= clause-name)))]
-                            [dest v]))))))
-     {}
-     named-clauses)))
+(def pos->getter
+  {:entity ds/datom->eid
+   :value ds/datom->val})
 
 (defn safe-first [thing]
   (when (vector? thing)
@@ -97,10 +47,10 @@
     `#(ds/datom-attr-val= ((comp ~@locator-vec) %) ~a ~v)))
 
 (defmacro sprinkle-dbsp-on [datalog-query]
-  (let [{where-clauses# :where find-vars# :find} (query->map datalog-query)
-        named-clauses# (name-clauses where-clauses#)
-        variable-index# (index-variables named-clauses#)
-        adjacency-list# (build-adjacency-list named-clauses#)
+  (let [{where-clauses# :where find-vars# :find} (parser/query->map datalog-query)
+        named-clauses# (parser/name-clauses where-clauses#)
+        variable-index# (parser/index-variables named-clauses#)
+        adjacency-list# (parser/build-adjacency-list named-clauses#)
         adjacency-tuples# (for [[from-node destinations] adjacency-list#
                                 [to-node _] destinations];
                             [from-node to-node])
@@ -123,8 +73,8 @@
                 (xforms/reduce (zs/zset-xf+ (map
                                              (xf/with-meta-f
                                                (juxt ~@(map (fn [find-var#]
-                                                              (let [[[clause-to-select# getter#] & _] (find-var# variable-index#)]
-                                                                `(comp ~getter# ~@(clause-to-select# locators#))))
+                                                              (let [[[clause-to-select# position#] & _] (find-var# variable-index#)]
+                                                                `(comp ~(position# pos->getter) ~@(clause-to-select# locators#))))
                                                             find-vars#))))))))
 
             ;; Stack overflow
@@ -147,9 +97,9 @@
                        `#(ds/datom-attr= % ~a2#)
                        `#(ds/datom-attr-val= % ~a2# ~v2#))
                   new-join `(xf/join-xf ~p1
-                                        (comp ~(get-in variable-index# [common-var# from#]) ~@(from# locators#))
+                                        (comp ~((get-in variable-index# [common-var# from#]) pos->getter) ~@(from# locators#))
                                         ~p2
-                                        ~(get-in variable-index# [common-var# to#])
+                                        ~((get-in variable-index# [common-var# to#]) pos->getter)
                                         ~state
                                         :last? ~(= #{to#} remaining-nodes#))]
               (recur (conj preds# p1 p2)

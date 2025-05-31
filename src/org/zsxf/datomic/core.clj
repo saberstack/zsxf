658e84441919837aadb3570078a5d1e2514b6e5c
Rangel Spasov <raspasov@gmail.com>
2025-05-30 23:45:02 -0700

    datomic cdc wip

--- src/org/zsxf/datomic/core.clj
+++ src/org/zsxf/datomic/core.clj
@@ -3,9 +3,12 @@
    [datascript.core :as d]
    [datascript.db :as ddb]
    [datomic.api :as dd]
+   [net.cgrand.xforms :as xforms]
+   [ss.loop]
    [clojure.core.async :as a]
    [org.zsxf.datom :as d2]
-   [taoensso.timbre :as timbre]))
+   [taoensso.timbre :as timbre])
+  (:import (clojure.lang IFn)))
 
 (defn db-uri [db-name]
   (str "datomic:sql://" db-name "?jdbc:sqlite:./datomic/storage/sqlite.db"))
@@ -42,8 +45,8 @@
            (let [a' (get idents-m a)]
              ;datom
              ;(ddb/datom e a' v t tf)
-             [e a' v t tf]
-             datom)))
+             ;datom
+             [e a' v t tf])))
     data))
 
 ;Incremental CDC:
@@ -51,20 +54,58 @@
 ; 2. Call get-log-transactions with (inc t)
 ; 3. Repeat
 
+(defonce cdc-ch (atom (a/chan 42)))
+(defonce cdc-last-t (atom nil))
+
+(defn ->zsxf-xf
+  "Transformation specific to ZSXF"
+  [idents-m]
+  (mapcat (fn [{:keys [data t id]}]
+            (tx-data->datoms idents-m data))))
+
+(defn ->reduce-to-chan [ch]
+  (fn
+    ([] ch)
+    ([accum-ch] accum-ch)
+    ([accum-ch item]
+     (timbre/info "reducing" item)
+     (a/>!! accum-ch item)
+     accum-ch)))
+
 (defn get-log-transactions
-  "- `conn`: The Datomic connection."
-  ([conn]
-   (get-log-transactions conn nil nil))
-  ([conn start end]
+  "Retrieves transactions from the Datomic log and processes them via a transducer.
+   Args:
+     conn - Datomic connection
+     reducing-f - A reducing function that processes the transformed transactions
+     ->xf - Function that takes an idents map and returns a transducer for transforming transactions
+     start - Optional start time/transaction ID to retrieve logs from (default nil)
+     end - Optional end time/transaction ID to retrieve logs until (default nil)
+  "
+  ([conn reducing-f] (get-log-transactions conn reducing-f (fn [_idents-m] (map identity)) nil nil))
+  ([conn reducing-f ->xf] (get-log-transactions conn reducing-f ->xf nil nil))
+  ([conn reducing-f ->xf start end]
    (let [log          (dd/log conn)
          idents-m     (into {} (get-all-idents conn))
          transactions (dd/tx-range log start end)]
-     ;TODO WIP
-     (sequence
-       (mapcat
-         (fn [{:keys [data t]}]
-           (tx-data->datoms idents-m data)))
+     (transduce
+       (comp
+         (map (fn [tx-m]
+                ; Store the last seen transaction ID :t in a volatile
+                (reset! cdc-last-t (:t tx-m))
+                ;return tx-m unchanged
+                tx-m))
+         (->xf idents-m))
+       reducing-f
        transactions))))
+
+(defn start-datomic-cdc!
+  []
+  (reset! cdc-ch (a/chan 10))
+  #_(ss.loop/go-loop ^{:id :datomic-cdc-loop}
+      [i 0]
+      (timbre/info "looping..." i)
+      (a/<! (a/timeout 1000))
+      (recur (inc i))))
 
 (defn react-on-transaction! [conn on-transaction]
   (let [tx-queue (dd/tx-report-queue conn)]
@@ -80,12 +121,21 @@
   "Initial naive implementation. No listeners or change data capture.
   Read all transactions datoms."
   [query conn]
-  (get-log-transactions conn))
+  ;TODO WIP
+  )
 
 (comment
 
   (let [conn (dd/connect (db-uri "mbrainz"))]
-    (take 2 (get-log-transactions conn)))
+    (take 100 (get-log-transactions conn conj ->zsxf-xf)))
+
+  (let [conn (dd/connect (db-uri "mbrainz"))]
+    (reset! cdc-ch (a/chan 1000))
+    (take 1 (get-log-transactions conn (->reduce-to-chan @cdc-ch) ->zsxf-xf)))
+
+  (let [conn (dd/connect (db-uri "mbrainz"))]
+    (reset! cdc-ch (a/chan 1000))
+    (take 1 (get-log-transactions conn (xforms/count conj) ->zsxf-xf)))
 
   (let [conn (dd/connect (db-uri "mbrainz"))]
     (react-on-transaction! conn (fn [txn] (timbre/info "txn:" txn))))
@@ -97,13 +147,10 @@
 
   (dd/connect (db-uri "zsxf"))
 
-  (get-log-transactions conn)
-
-
   (def conn (dd/connect (db-uri "zsxf")))
 
 
-  (get-all-idents (dd/connect (db-uri "zsxf")))
+  (get-all-idents (dd/connect (db-uri "mbrainz")))
 
   ;return all datoms in the db (including internal setup datoms)
   (into []

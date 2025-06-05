777c5eede0b808c1fe366cd2786faa14504106c0
Nick McAvoy <ohbadiah@outlook.com>
2025-06-05 18:25:46 -0400

    Incorporate batting stats (home runs) into baseball ETL

--- test/org/zsxf/test_data/baseball_etl.clj
+++ test/org/zsxf/test_data/baseball_etl.clj
@@ -12,16 +12,17 @@
 
 (def ds (jdbc/get-datasource db-spec))
 
-
 (defn query [q]
   (jdbc/execute! ds (sql/format q)
                  {:builder-fn rs/as-unqualified-maps}))
 
-(defn get-players [& {:keys [limit hall-of-fame-only?]}]
+(defn get-players [& {:keys [limit hall-of-fame-only? min-year max-year]}]
   (let [base-query (-> (h/select :p.playerID :p.nameFirst :p.nameLast
                                  :p.debut :p.finalGame :p.birthYear)
                        (h/from [:People :p]))]
     (cond-> base-query
+      min-year (h/where [:or [:>= :p.debut min-year] [:>= :p.finalGame min-year]])
+      max-year (h/where [:or [:< :p.debut max-year] [:< :p.finalGame max-year]])
       hall-of-fame-only? (-> (h/join [:HallOfFame :hof] [:= :p.playerID :hof.playerID])
                              (h/where [:= :hof.inducted "Y"]))
       limit (h/limit limit)
@@ -32,7 +33,7 @@
                        (h/from :Teams))]
     (cond-> base-query
       min-year (h/where [:>= :yearID min-year])
-      max-year (h/where [:<= :yearID max-year])
+      max-year (h/where [:< :yearID max-year])
       limit (h/limit limit)
       true query)))
 
@@ -49,6 +50,29 @@
       limit (h/limit limit)
       true query)))
 
+;; Get batting statistics
+;; Note: We intentionally ignore the stint column. In rare cases (76 in baseball history),
+;; a player may have multiple stints with the same team in one year. We just take the
+;; first stint's data for simplicity. This won't affect home run leader calculations
+;; meaningfully, as the cases are rare and the differences are small.
+(defn get-batting [& {:keys [min-year max-year player-ids team-ids limit]}]
+  (let [base-query (-> (h/select :playerID :teamID :yearID :HR
+                                 ;:G :AB :R :H
+                                 ;[:2B :doubles]
+                                 ;[:3B :triples]
+                                 ;:RBI :SB :CS :BB :SO
+                                 )
+                       (h/from :Batting)
+                       ;; Just order by player/team/year, ignoring stint
+                       (h/order-by :playerID :teamID :yearID))]
+    (cond-> base-query
+      min-year (h/where [:>= :yearID min-year])
+      max-year (h/where [:< :yearID max-year])
+      player-ids (h/where [:in :playerID player-ids])
+      team-ids (h/where [:in :teamID team-ids])
+      limit (h/limit limit)
+      true query)))
+
 (def schema
   {:player/id {:db/cardinality :db.cardinality/one
                :db/unique :db.unique/identity}
@@ -58,9 +82,10 @@
              :db/unique :db.unique/identity}
    :team/name {}
 
-   :tenure/player {:db/type :db.type/ref}
-   :tenure/team {:db/type :db.type/ref}
-   :tenure/year {:db/cardinality :db.cardinality/many}})
+   :season/player {:db/type :db.type/ref}
+   :season/team {:db/type :db.type/ref}
+   :season/year {}
+   :season/home-runs {}})
 
 (defn fresh-conn []
   (d/create-conn schema))
@@ -96,33 +121,29 @@
      (get-players opts)))
 
   (transduce
-   (comp (map (fn [[first-appearance & _ :as appearances]]
-                (assoc first-appearance :years (map :yearID appearances))))
-         (map (fn [{player-id :playerID team-id :teamID years :years :as row}]
+   (comp (map (fn [{player-id :playerID team-id :teamID year :yearID home-runs :HR :as row}]
                 (let [{player-eid :db/id}  (d/entity @conn [:player/id player-id])
-                      {team-eid :db/id}  (d/entity @conn [:team/id team-id])]
-                  (when (and player-eid team-eid)
-                    {:tenure/player player-eid
-                     :tenure/team team-eid
-                     :tenure/year years}))))
+                         {team-eid :db/id}  (d/entity @conn [:team/id team-id])]
+                     (when (and player-eid team-eid)
+                       {:season/player player-eid
+                        :season/team team-eid
+                        :season/year year
+                        :season/home-runs home-runs}))))
          (remove nil?)
          (partition-all 100))
    (completing (fn [_ batch]
                  (d/transact! conn batch)))
    nil
-   (->> (get-appearances opts)
-         (group-by (juxt :playerID :teamID))
-         vals)))
+   (get-batting opts)))
 
 (def conn-registry (atom {}))
 
 (defn conn-for-range [low high]
-  (if (contains? @conn-registry [low high])
-    (get @conn-registry [low high])
-    (let [conn (fresh-conn)]
-      (populate-datascript-db conn {:min-year low :max-year high})
-      (swap! conn-registry assoc [low high] conn)
-      conn)))
+  (or (get @conn-registry [low high])
+      (let [conn (fresh-conn)]
+        (populate-datascript-db conn {:min-year low :max-year high})
+        (swap! conn-registry assoc [low high] conn)
+        conn)))
 
 (comment
   (time (do

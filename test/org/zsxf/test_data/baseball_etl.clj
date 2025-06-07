d42bbf924df4c38e57287eb49c0240f5f1ffd551
Nick McAvoy <ohbadiah@outlook.com>
2025-06-07 15:02:47 -0400

    Scratch code for playing around with aggregate behavior

--- test/org/zsxf/test_data/baseball_etl.clj
+++ test/org/zsxf/test_data/baseball_etl.clj
@@ -143,7 +143,7 @@
 (def conn-registry (atom {}))
 
 (defn conn-for-range [low high]
-  (or #_(get @conn-registry [low high])
+  (or (get @conn-registry [low high])
       (let [conn (fresh-conn)]
         (populate-datascript-db conn {:min-year low :max-year high})
         (swap! conn-registry assoc [low high] conn)
@@ -184,7 +184,7 @@
                       (reduce +)))))
     (testing "ZSXF gives the \"expected\" behavior."
       (let [zquery (q/create-query
-                    (static-compile '[:find ?player-name (sum ?home-runs)
+                    (static-compile '[:find ?player-name ?year  ?home-runs
                                       :where
                                       [?p :player/id "aaronha01"]
                                       [?p :player/name ?player-name]
@@ -195,7 +195,145 @@
         (is (= {["Hank Aaron"] #{['?home-runs 755]}}
                (q/get-aggregate-result zquery)))))))
 
+(def za (atom []))
+(def zquery (q/create-query
+             (fn [state43087]
+               (comp
+                (org.zsxf.xf/mapcat-zset-transaction-xf)
+                (map
+                 (fn [zset__42554__auto__]
+                   (org.zsxf.xf/disj-irrelevant-items
+                    zset__42554__auto__
+                    #(org.zsxf.datom/datom-attr= % :season/home-runs)
+                    #(org.zsxf.datom/datom-attr-val= % :player/id "aaronha01")
+                    #(org.zsxf.datom/datom-attr= % :season/player)
+                    #(org.zsxf.datom/datom-attr= % :season/year)
+                    #(org.zsxf.datom/datom-attr= % :player/name))))
+                (org.zsxf.xf/join-xf
+                 {:path identity,
+                  :pred
+                  #(org.zsxf.datom/datom-attr-val= % :player/id "aaronha01"),
+                  :index-kfn org.zsxf.datom/datom->eid,
+                  :clause '[?p :player/id "aaronha01"]}
+                 {:path identity,
+                  :pred #(org.zsxf.datom/datom-attr= % :player/name),
+                  :index-kfn org.zsxf.datom/datom->eid,
+                  :clause '[?p :player/name ?player-name]}
+                 state43087)
+                (org.zsxf.xf/join-xf
+                 {:path org.zsxf.datalog.macro-util/safe-first,
+                  :pred
+                  #(org.zsxf.datom/datom-attr-val= % :player/id "aaronha01"),
+                  :index-kfn org.zsxf.datom/datom->eid,
+                  :clause '[?p :player/id "aaronha01"]}
+                 {:path identity,
+                  :pred #(org.zsxf.datom/datom-attr= % :season/player),
+                  :index-kfn org.zsxf.datom/datom->val,
+                  :clause '[?s :season/player ?p]}
+                 state43087)
+                (org.zsxf.xf/join-xf
+                 {:path org.zsxf.datalog.macro-util/safe-second,
+                  :pred #(org.zsxf.datom/datom-attr= % :season/player),
+                  :index-kfn org.zsxf.datom/datom->eid,
+                  :clause '[?s :season/player ?p]}
+                 {:path identity,
+                  :pred #(org.zsxf.datom/datom-attr= % :season/home-runs),
+                  :index-kfn org.zsxf.datom/datom->eid,
+                  :clause '[?s :season/home-runs ?home-runs]}
+                 state43087)
+                (org.zsxf.xf/join-xf
+                 {:path
+                  (comp
+                   org.zsxf.datalog.macro-util/safe-second
+                   org.zsxf.datalog.macro-util/safe-first),
+                  :pred #(org.zsxf.datom/datom-attr= % :season/player),
+                  :index-kfn org.zsxf.datom/datom->eid,
+                  :clause '[?s :season/player ?p]}
+                 {:path identity,
+                  :pred #(org.zsxf.datom/datom-attr= % :season/year),
+                  :index-kfn org.zsxf.datom/datom->eid,
+                  :clause '[?s :season/year ?year]}
+                 state43087
+                 :last?
+                 true)
+                (net.cgrand.xforms/reduce
+                 (org.zsxf.zset/zset-xf+
+                  (map
+                   (org.zsxf.xf/same-meta-f
+                    (juxt
+                     (comp
+                      org.zsxf.datom/datom->val
+                      org.zsxf.datalog.macro-util/safe-second
+                      org.zsxf.datalog.macro-util/safe-first
+                      org.zsxf.datalog.macro-util/safe-first
+                      org.zsxf.datalog.macro-util/safe-first)
+                     (comp
+                      org.zsxf.datom/datom->val
+                      org.zsxf.datalog.macro-util/safe-second)
+                     (comp
+                      org.zsxf.datom/datom->val
+                      org.zsxf.datalog.macro-util/safe-second
+                      org.zsxf.datalog.macro-util/safe-first))))))))))
+
+(defn run-zquery []
+  (reset! za [])
+  (let [conn (conn-for-range 1950 2000)
+        _ (ds/init-query-with-conn zquery conn)
+        result (q/get-result zquery)]
+    (ds/unlisten-all! conn)
+    result))
+
+
 (comment
+  (def aaa (run-zquery))
+  (#(nth % 2) (first aaa))
+
+org.zsxf.zset/zset+
+(set! *print-meta* false)
+(org.zsxf.zset/zset+ (map (org.zsxf.xf/same-meta-f (juxt #(nth % 2))) ) #{} aaa)
+(transduce (org.zsxf.zset/zset-xf+
+             (map (org.zsxf.xf/same-meta-f (juxt #(nth % 2))) ))
+           conj
+           #{}
+           aaa)
+  (into #{} (org.zsxf.zset/zset-xf+
+             (map (org.zsxf.xf/same-meta-f (juxt #(nth % 2))) )) aaa)#{^#:zset{:w 1024} ["Hank Aaron" 1958 30] ^#:zset{:w 1024} ["Hank Aaron" 1971 47] ^#:zset{:w 1024} ["Hank Aaron" 1974 20] ^#:zset{:w 1024} ["Hank Aaron" 1964 24] ^#:zset{:w 1024} ["Hank Aaron" 1972 34] ^#:zset{:w 1024} ["Hank Aaron" 1959 39] ^#:zset{:w 1024} ["Hank Aaron" 1969 44] ^#:zset{:w 1024} ["Hank Aaron" 1961 34] ^#:zset{:w 1024} ["Hank Aaron" 1967 39] ^#:zset{:w 1024} ["Hank Aaron" 1973 40] ^#:zset{:w 1024} ["Hank Aaron" 1956 26] ^#:zset{:w 1024} ["Hank Aaron" 1970 38] ^#:zset{:w 1024} ["Hank Aaron" 1975 12] ^#:zset{:w 1024} ["Hank Aaron" 1968 29] ^#:zset{:w 1024} ["Hank Aaron" 1954 13] ^#:zset{:w 1024} ["Hank Aaron" 1962 45] ^#:zset{:w 1024} ["Hank Aaron" 1963 44] ^#:zset{:w 1024} ["Hank Aaron" 1955 27] ^#:zset{:w 1024} ["Hank Aaron" 1957 44] ^#:zset{:w 1024} ["Hank Aaron" 1960 40] ^#:zset{:w 1024} ["Hank Aaron" 1976 10] ^#:zset{:w 1024} ["Hank Aaron" 1965 32] ^#:zset{:w 1024} ["Hank Aaron" 1966 44]}
+  (map (org.zsxf.xf/same-meta-f (juxt #(nth % 2))) aaa)
+  (transduce
+   )
+  ((net.cgrand.xforms/reduce
+    (org.zsxf.zset/zset-xf+ (map #(nth % 2)))) aaa)
+  (org.zsxf.zset/index aaa first)
+  (count @za)
+
+  (d/q '[:find ?player-name (sum ?home-runs)
+         :where
+         [?p :player/id "aaronha01"]
+         [?p :player/name ?player-name]
+         [?s :season/player ?p]
+         [?s :season/home-runs ?home-runs]
+         [?s :season/year ?year]]
+       (-> (conn-for-range 1950 2000) deref)
+   )
+  (static-compile '[:find ?player-name (sum ?home-runs)
+         :where
+         [?p :player/id "aaronha01"]
+         [?p :player/name ?player-name]
+         [?s :season/player ?p]
+         [?s :season/home-runs ?home-runs]
+         [?s :season/year ?year]])
+
+  org.zsxf.zset/zset-xf+
+  (org.zsxf.zset/zset+ )
+  org.zsxf.zset/index
+  org.zsxf.xf/group-by-xf
+  (static-compile '[:find ?player-name ?home-runs
+         :where
+         [?p :player/id "aaronha01"]
+         [?p :player/name ?player-name]
+         [?s :season/player ?p]
+         [?s :season/home-runs ?home-runs]
+         [?s :season/year ?year]])
   (time (do
           (let [opts {:min-year 1980
                       :max-year 1985}]

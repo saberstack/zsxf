4b01452e5febae83a6fae5a8ee84680b5a7abbbe
Rangel Spasov <raspasov@gmail.com>
2025-07-16 01:38:11 -0700

    datomic.clj: on-demand setup steps, WIP

--- src/org/zsxf/setup/datomic.clj
+++ src/org/zsxf/setup/datomic.clj
@@ -6,3 +6,8 @@
   "Create SQLite database file at the specified path."
   [path]
   (process/shell "sqlite3" path "-init" "resources/datomic/sqlite_init.sql" ".exit"))
+
+;TODO Continue here
+
+; - create sqlite db
+; - create transactor.properties config with sqlite path

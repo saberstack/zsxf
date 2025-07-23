add242512a5d1e2a19f037201c2ebfb70a753ef7
Rangel Spasov <raspasov@gmail.com>
2025-07-23 02:19:42 -0700

    src, deps.edn: Datomic HN import, WIP

--- src/org/saberstack/datomic/import.clj
+++ src/org/saberstack/datomic/import.clj
@@ -3,11 +3,16 @@
             [charred.api :as charred]
             [clojure.core.async :as a]
             [clojure.string :as str]
+            [datomic.api :as dd]
+            [org.zsxf.datomic.cdc :as datomic-cdc]
             [org.zsxf.util :as util]
-            [taoensso.nippy :as nippy]))
+            [taoensso.nippy :as nippy]
+            [taoensso.timbre :as timbre]))
 
-(defonce tmp-hn-items (atom []))
+(defonce hn-items (atom []))
 
+;; Load from disk all item files
+;; The files are named like "items-500-1000.parquet"
 (defn- id-to-sort-by [unix-path]
   (parse-long
     (peek
@@ -19,19 +24,179 @@
       (filter (fn [unix-path] (str/starts-with? unix-path "./hndl/items-")))
       (fs/list-dir "./hndl"))))
 
+(def file-xf
+  (comp
+    (map (fn [unix-path] (str unix-path)))
+    (mapcat (fn [file] (nippy/thaw-from-file file)))
+    (map (fn [s] (charred/read-json s :key-fn keyword)))))
+
 (defn thaw-item-files [files]
-  (reset! tmp-hn-items [])
+  (reset! hn-items [])
   (System/gc)
   (let [input-ch (util/pipeline-output
                    ;write to atom, parquet, etc
-                   (map (fn [item] (swap! tmp-hn-items conj item)))
+                   (map (fn [item] (swap! hn-items conj item)))
                    ;parallel transform
-                   (comp
-                     (map (fn [unix-path] (str unix-path)))
-                     (mapcat (fn [file] (nippy/thaw-from-file file)))
-                     (map (fn [s] (charred/read-json s :key-fn keyword)))))]
+                   file-xf)]
     (a/onto-chan!! input-ch files)))
 
 (comment
   (thaw-item-files (all-item-files))
-  (count @tmp-hn-items))
+  (count @hn-items))
+
+;; End of section
+
+(def datomic-schema
+  [{:db/ident       :hn.item/id
+    :db/valueType   :db.type/long
+    :db/cardinality :db.cardinality/one
+    :db/unique      :db.unique/identity
+    :db/doc         "The item's unique id from HackerNews."}
+
+   {:db/ident       :hn.item/deleted
+    :db/valueType   :db.type/boolean
+    :db/cardinality :db.cardinality/one
+    :db/doc         "true if the item is deleted."}
+
+   {:db/ident       :hn.item/type
+    :db/valueType   :db.type/keyword
+    :db/cardinality :db.cardinality/one
+    :db/index       true
+    :db/doc         "The type of item. One of :job, :story, :comment, :poll, or :pollopt."}
+
+   {:db/ident       :hn.item/by
+    :db/valueType   :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/index       true
+    :db/doc         "The username of the item's author."}
+
+   {:db/ident       :hn.item/time
+    :db/valueType   :db.type/instant
+    :db/cardinality :db.cardinality/one
+    :db/doc         "Creation date of the item."}
+
+   {:db/ident       :hn.item/text
+    :db/valueType   :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc         "The comment, story or poll text. Can contain HTML."}
+
+   {:db/ident       :hn.item/dead
+    :db/valueType   :db.type/boolean
+    :db/cardinality :db.cardinality/one
+    :db/doc         "true if the item is dead."}
+
+   {:db/ident       :hn.item/parent
+    :db/valueType   :db.type/ref
+    :db/cardinality :db.cardinality/one
+    :db/doc         "The comment's parent: either another comment or the relevant story."}
+
+   {:db/ident       :hn.item/poll
+    :db/valueType   :db.type/ref
+    :db/cardinality :db.cardinality/one
+    :db/doc         "The pollopt's associated poll."}
+
+   {:db/ident       :hn.item/kids
+    :db/valueType   :db.type/ref
+    :db/cardinality :db.cardinality/many
+    :db/doc         "The ids of the item's comments, in ranked display order."}
+
+   {:db/ident       :hn.item/url
+    :db/valueType   :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc         "The URL of the story."}
+
+   {:db/ident       :hn.item/score
+    :db/valueType   :db.type/long
+    :db/cardinality :db.cardinality/one
+    :db/doc         "The story's score, or the votes for a pollopt."}
+
+   {:db/ident       :hn.item/title
+    :db/valueType   :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/fulltext    true
+    :db/doc         "The title of the story, poll or job. Can contain HTML."}
+
+   {:db/ident       :hn.item/parts
+    :db/valueType   :db.type/ref
+    :db/cardinality :db.cardinality/many
+    :db/doc         "A list of related pollopts, in display order."}
+
+   {:db/ident       :hn.item/descendants
+    :db/valueType   :db.type/long
+    :db/cardinality :db.cardinality/one
+    :db/doc         "In the case of stories or polls, the total comment count."}])
+
+(defn delete-and-init-datomic! []
+  (let [^String db-uri (datomic-cdc/uri-sqlite "hackernews")
+        _              (dd/delete-database db-uri)
+        _              (dd/create-database db-uri)
+        conn           (dd/connect db-uri)
+        _              (dd/transact conn datomic-schema)]
+    conn))
+
+(defn hn-conn []
+  (let [^String db-uri (datomic-cdc/uri-sqlite "hackernews")]
+    (dd/connect db-uri)))
+
+(defn- hn-item->tx-data
+  "Transforms a HackerNews item map into a Datomic transaction map."
+  [item]
+  (cond-> {:hn.item/id (:id item)}
+    (contains? item :deleted) (assoc :hn.item/deleted (:deleted item))
+    (contains? item :type) (assoc :hn.item/type (keyword (:type item)))
+    (contains? item :by) (assoc :hn.item/by (:by item))
+    (contains? item :time) (assoc :hn.item/time (java.util.Date. (* 1000 (:time item))))
+    (contains? item :text) (assoc :hn.item/text (:text item))
+    (contains? item :dead) (assoc :hn.item/dead (:dead item))
+    ;(contains? item :parent) (assoc :hn.item/parent [:hn.item/id (:parent item)])
+    ;(contains? item :poll)    (assoc :hn.item/poll [:hn.item/id (:poll item)])
+    ;(contains? item :kids)    (assoc :hn.item/kids (mapv (fn [id] [:hn.item/id id]) (:kids item)))
+    (contains? item :url) (assoc :hn.item/url (:url item))
+    (contains? item :score) (assoc :hn.item/score (:score item))
+    (contains? item :title) (assoc :hn.item/title (:title item))
+    ;(contains? item :parts)   (assoc :hn.item/parts (mapv (fn [id] [:hn.item/id id]) (:parts item)))
+    (contains? item :descendants) (assoc :hn.item/descendants (:descendants item))))
+
+(defn hn-item-tx-data-xf [chunk-size]
+  (comp
+    (map hn-item->tx-data)
+    (partition-all chunk-size)
+    (take 1000)))
+
+(comment
+  (set! *print-namespace-maps* false)
+  (into []
+    (hn-item-tx-data-xf 100)
+    @hn-items)
+  )
+
+(defn import-items-to-datomic! [conn]
+  (let [items @hn-items]
+    ;; Transact in chunks to avoid overwhelming the transactor.
+    (transduce
+      (hn-item-tx-data-xf 1000)
+      (completing
+        (fn [conn chunk]
+          @(dd/transact conn chunk)
+          conn))
+      conn
+      items)))
+
+(defn count-number-of-comments []
+  (let [conn (hn-conn)]
+    (dd/q
+      '[:find (count ?e)
+        :in $
+        :where [?e :hn.item/id _]]
+      (dd/db conn)))
+  )
+
+(comment
+
+  (delete-and-init-datomic!)
+
+  (hn-conn)
+
+  (time
+    (import-items-to-datomic! (hn-conn)))
+  )

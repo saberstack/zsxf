cb8bdb07d87b2f47adf165042d876203f4d8496f
Rangel Spasov <raspasov@gmail.com>
2025-08-27 06:11:46 -0700

    datasync.hn: improve item syncing, working state

--- src/org/saberstack/datasync/hn.clj
+++ src/org/saberstack/datasync/hn.clj
@@ -18,13 +18,27 @@
 (defonce sleep-ms (atom 500))
 (defonce last-dl (atom nil))
 
+(defn- body->id [^String body]
+  (ss.io/try-io!
+    (-> body (charred/read-json :key-fn keyword) :id long)))
+
 (defn get-parsed-items! [item-ids]
-  (let [resps (ss.io/try-io! (doall (pmap api/item! item-ids)))]
-    {:item-ids      item-ids
-     :items         (into [] (map :body) resps)
-     :status-200?   (every? #(= 200 %) (map :status resps))
-     :reached-null? (some? (medley/find-first #(= "null" %) (map :body resps)))
-     :resps         resps}))
+  (let [null-pred     #(= "null" %)
+        resps         (ss.io/try-io! (into [] (pmap api/item! item-ids)))
+        items         (into [] (comp (map :body) (medley/take-upto null-pred)) resps)
+        reached-null? (some? (medley/find-first null-pred items))
+        items'        (if reached-null?
+                        (vec (butlast (medley/take-upto null-pred items)))
+                        items)
+        first-synced  (body->id (first items'))
+        last-synced   (body->id (peek items'))]
+    {:status-200?   (every? #(= 200 %) (map :status resps))
+     :item-ids      item-ids
+     :items         items
+     :reached-null? reached-null?
+     :resps         resps
+     :first-synced  first-synced
+     :last-synced   last-synced}))
 
 (defn items-to-disk! [items start-id end-id]
   (ss.io/try-io!
@@ -32,60 +46,71 @@
       (str "./hndl/" "items-" start-id "-" end-id)
       items)))
 
-(defn last-item-id-to-disk! [max-item-id]
+(defn write-last-item-id-to-disk! [item-id]
   (ss.io/try-io!
     (nippy/freeze-to-file
       path-last-item-id
-      max-item-id)))
+      item-id)))
 
 (defn get-last-item-id-from-disk! []
-  (nippy/thaw-from-file
-    path-last-item-id))
+  (ss.io/try-io!
+    (nippy/thaw-from-file
+      path-last-item-id)))
 
-(defn get-hn-max-item!
+(defn get-hn-max-item-id!
   "Get and parse HN max item."
   []
-  (let [x "test"]
-    (ss.io/try-io!
-      (-> (api/max-item!) :body parse-long))))
+  (ss.io/try-io!
+    (-> (api/max-item!) :body parse-long)))
 
-(defn start-sync-items! [item-id-start item-id-max]
+(defn sync-items! [prev-end end]
   (reset! halt? false)
   (transduce
     (comp
       (partition-all 1000)
       (map get-parsed-items!)
-      (map (fn [{:keys [items item-ids status-200? reached-null? resps] :as m}]
+      (map (fn [{:keys [items item-ids status-200? resps first-synced last-synced] :as m}]
              (if status-200?
                (do
-                 (items-to-disk! items (first item-ids) (peek item-ids))
-                 (reset! last-dl [(first item-ids) (peek item-ids)])
-                 (assoc m :download-ok? true))
-               (do
-                 (reset! stopped-at resps)
-                 (assoc m :download-ok? false)))))
-      (halt-when (fn [{:keys [download-ok? reached-null?]}]
+                 (when (and (int? first-synced) (int? last-synced))
+                   (items-to-disk! items first-synced last-synced)
+                   (write-last-item-id-to-disk! last-synced)
+                   (timbre/info [::sync-ok [first-synced last-synced]]))
+                 (reset! last-dl [(first item-ids) (peek item-ids)]))
+               ;else
+               (reset! stopped-at resps))
+             ;return
+             m))
+      (halt-when (fn [{:keys [status-200? reached-null? last-synced]}]
                    (or
-                     (not download-ok?)
+                     (not status-200?)
+                     (nil? last-synced)
                      (true? reached-null?)
                      (true? @halt?))))
-      (map (fn [_] (Thread/sleep ^long @sleep-ms))))
+      (map (fn [_] :ok)))
     conj
-    (iterate inc item-id-start)))
+    (range (inc prev-end) (inc end))))
 
 (timbre/set-ns-min-level! :debug)
 
 (defn start-refresh-task []
   (tt/every! 5
     (bound-fn []
-      (timbre/info "I don't refresh anything, yet")
-      (let [max-item-id (get-hn-max-item!)]
-        (when (int? max-item-id)
-          (last-item-id-to-disk! max-item-id))))))
+      (let [item-id-last-synced (get-last-item-id-from-disk!)
+            item-id-max         (get-hn-max-item-id!)]
+        (if (and
+              (int? item-id-max)
+              (int? item-id-last-synced)
+              (< item-id-last-synced item-id-max))
+          (do
+            (timbre/info [::next [item-id-last-synced item-id-max]])
+            (timbre/info (sync-items! item-id-last-synced item-id-max)))
+          (timbre/info [::wait [item-id-last-synced item-id-max]]))))))
 
 ;REPL task control
 (comment
   (tt/reset-tasks!)
+  (tt/start!)
   (start-refresh-task)
   (:body (api/max-item!))
 
@@ -98,7 +123,7 @@
   (reset! halt? true)
   (reset! sleep-ms 20)
   (future
-    (start-sync-items! 44642001)))
+    (sync-items! 44642001)))
 
 (comment
   (nippy/freeze-to-file
